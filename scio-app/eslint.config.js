4461bda2d8a99db973d5557dda6854304f0b19f1
grdddj <jiri.musil06@seznam.cz>
2025-09-08 19:08:13 +0200

    chore: switch to bun, install dev tools, include db dependencies

---
+++ scio-app/eslint.config.js
@@ -0,0 +1,17 @@
+import withNuxt from './.nuxt/eslint.config.mjs'
+
+export default withNuxt({
+  rules: {
+    'vue/multi-word-component-names': 'off',
+    'vue/no-v-html': 'warn',
+    '@typescript-eslint/no-explicit-any': 'off',
+    'vue/html-self-closing': 'off',
+    '@typescript-eslint/no-unused-vars': [
+      'warn',
+      {
+        argsIgnorePattern: '^_',
+        varsIgnorePattern: '^_',
+      },
+    ],
+  },
+})
